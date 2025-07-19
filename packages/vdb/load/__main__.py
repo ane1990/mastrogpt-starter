fb029b7da9b736c677342d308380c7a678773603
Alberto Anelli <a.ane90@gmail.com>
2025-07-19 11:33:50 +0000

    [L6] lesson six, first part

---
+++ packages/vdb/load/__main__.py
@@ -0,0 +1,13 @@
+#--kind python:default
+#--web true
+#--param OLLAMA_HOST $OLLAMA_HOST
+#--param OLLAMA_TOKEN $AUTH
+#--param MILVUS_HOST $MILVUS_HOST
+#--param MILVUS_PORT $MILVUS_PORT
+#--param MILVUS_DB_NAME $MILVUS_DB_NAME
+#--param MILVUS_TOKEN $MILVUS_TOKEN
+#--param COLLECTION alberto
+
+import load
+def main(args):
+  return { "body": load.load(args) }
